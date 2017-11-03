e3e3114e91e3a4e4cfd7c444795e430d5129d8af
Oleh_Bastun <Oleh_Bastun@epam.com>
2017-11-03 18:38:14 +0200

    net and compose ports

--- net.sh
+++ net.sh
@@ -4,6 +4,6 @@ docker network disconnect docj_default jenkins-master && echo jenkins-master dcd
 docker network disconnect docj_default artifactory && echo artifactory dcd
 docker network disconnect docj_default nginx-artifactory && echo nginx-artifactory dcd
 echo 'Connecting containers'
-docker network connect bridge jenkins-master && echo ok
-docker network connect bridge artifactory && echo ok
-docker network connect bridge nginx-artifactory && echo ok
+docker network connect bridge jenkins-master && echo jenkins-master ok
+docker network connect bridge artifactory && echo artifactory ok
+docker network connect bridge nginx-artifactory && echo nginx-artifactory ok
